f291dfb0514f2e14db22e8c2130e1ecfd47c46cc
Mickey Chen <poverwhelming@gmail.com>
2020-10-31 18:00:35 +0800

    added PostOption schema

---
+++ lib/estate/post/options.ex
@@ -0,0 +1,9 @@
+defmodule Estate.Post.Options do
+  use Estate, :schema
+
+  embedded_schema do
+    field :has_elevator, :boolean
+    field :decorated, :boolean
+    field :furished, :boolean
+  end
+end
