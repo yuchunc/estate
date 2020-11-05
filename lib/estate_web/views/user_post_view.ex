c755fd7d1acb6205e1c85e13cd6bbf5203b20390
Mickey Chen <poverwhelming@gmail.com>
2020-11-05 18:50:10 +0800

    added badges on cards for options

--- lib/estate_web/views/user_post_view.ex
+++ lib/estate_web/views/user_post_view.ex
@@ -3,4 +3,26 @@ defmodule EstateWeb.UserPostView do
 
   alias Estate.PreloadData
   alias Estate.Utils
+
+  @option_translation %{
+    has_parking: "含車位",
+    has_elevator: "有電梯"
+  }
+
+  def options_badge_text(%{__struct__: _} = opts) do
+    opts
+    |> Map.from_struct
+    |> options_badge_text()
+  end
+
+  def options_badge_text(opts) do
+    Enum.flat_map(opts, fn
+      {opt, true} -> [opt]
+      _ -> []
+    end)
+  end
+
+  def translate_option(opt) do
+    @option_translation[opt]
+  end
 end
